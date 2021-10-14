d4728c2eb5b37de2a5314e797ddf8e40f8ad3a1e
DylanToporek <dylantoporek@gmail.com>
2021-10-14 18:18:49 -0400

    readme added

--- node.js
+++ node.js
@@ -1,3 +1,4 @@
+//player submit-event listener
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector("#chooser")
   form.addEventListener('submit', (e) => {
@@ -8,16 +9,27 @@ document.addEventListener("DOMContentLoaded", () => {
     }) 
 })
 
+// player submit event-function
+function selector(pokemon){
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    .then(res => res.json())
+    .then(data => renderPokePlayer(data))
+}
+
+//like-event listener
 const likeButton = document.querySelector("#likeButton")
 likeButton.addEventListener('click', like)
 
+//like-event function
 function like(){
     alert('You liked the page!')
 }
 
+//cpu random button-event listener
 let cpuRandom = document.querySelector("#cpuRandomizer")
 cpuRandom.addEventListener('click', randomizeCPU)
 
+//cpu random button-event function
 function randomizeCPU(){
     fetch('https://pokeapi.co/api/v2/pokemon?limit=1200&offset=0')
     .then(res => res.json())
@@ -28,6 +40,110 @@ function randomizeCPU(){
         .then(data => renderPokeCPU(data))
 
     }) 
+}
+
+//player random button-event listener
+let random = document.querySelector("#randomizer")
+random.addEventListener('click', randomizePlayer)
+
+//player random event-function
+function randomizePlayer(){
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=1200&offset=0')
+    .then(res => res.json())
+    .then(data => {
+        let randomPoke = (data.results[Math.floor(Math.random() * data.results.length)])
+        fetch(randomPoke.url)
+        .then(res => res.json())
+        .then(data => renderPokePlayer(data))
+
+    })
+}
+
+//battle button event listener
+let battle = document.querySelector("#battle")
+battle.addEventListener('click', calculateWinner)
+
+//battle button event function
+function calculateWinner(){
+    checkWinnerByType()
+    
+    if(playerWins === cpuWins){
+        alert('No winner by type. Let\'s check their weights!')
+        checkWinnerByWeight()
+    }
+    checkScore()
+}
+
+////////////Helper Functions////////////////
+
+let cpuScore = 0
+let playerScore = 0
+
+function checkScore(){
+    const winningScore = 7
+    if (cpuScore >= winningScore){
+    alert('You lose. Try again!')
+        location.reload()
+    }
+    if (playerScore >= winningScore){
+        alert('Congratulations, you won! Play again?')
+        location.reload()
+    }
+}
+
+function checkWinnerByType(){
+    let playerScoreContainer = document.querySelector("#playerScore")
+    let cpuScoreContainer = document.querySelector("#cpuScore")
+    let playerPokeType = document.querySelector("#playerType").innerHTML
+    let cpuPokeType = document.querySelector("#cpuType").innerHTML
+    let playerPokeTypeArr = typeChart[playerPokeType]
+    let cpuPokeTypeArr = typeChart[cpuPokeType]
+    
+    if (cpuWins = playerPokeTypeArr.includes(cpuPokeType)){
+        cpuScore = cpuScore + 1
+        document.querySelector("#displayPlayer1").innerHTML = ""
+        cpuScoreContainer.innerHTML = cpuScore
+        alert(`You lose: ${cpuPokeType} beats ${playerPokeType}!`)
+    }
+    
+    if (playerWins = cpuPokeTypeArr.includes(playerPokeType)){
+        playerScore = playerScore + 1
+        document.querySelector("#displayCPU").innerHTML = ''
+        playerScoreContainer.innerHTML = playerScore
+        alert(`You win: ${playerPokeType} beats ${cpuPokeType}!`)
+    }
+}
+
+function checkWinnerByWeight(){
+    let playerScoreContainer = document.querySelector("#playerScore")
+    let cpuScoreContainer = document.querySelector("#cpuScore")
+    let playerWeight = document.querySelector("#playerWeight").innerHTML
+    let cpuWeight = document.querySelector("#cpuWeight").innerHTML
+
+    if(parseInt(playerWeight) > parseInt(cpuWeight)){
+        playerScore = playerScore + 1
+        document.querySelector("#displayCPU").innerHTML = ''
+        playerScoreContainer.innerHTML = playerScore
+        let cpuWKG = (cpuWeight / 10)
+        let playerWKG = (playerWeight / 10)
+        alert(`You win: ${playerWKG} kg > ${cpuWKG} kg!`)
+    }
+
+    if (parseInt(cpuWeight) > parseInt(playerWeight)){
+        cpuScore = cpuScore + 1
+        document.querySelector("#displayPlayer1").innerHTML = ""
+        cpuScoreContainer.innerHTML = cpuScore
+        let cpuWKG = (cpuWeight / 10)
+        let playerWKG = (playerWeight / 10)
+        alert(`You lose: ${cpuWKG} kg > ${playerWKG} kg!`)
+    }
+
+    if (parseInt(cpuWeight) === parseInt(playerWeight)){
+        document.querySelector("#displayPlayer1").innerHTML = ""
+        document.querySelector("#displayCPU").innerHTML = ""
+        alert('What are the odds? No winner by type or weight!')
+    }
+
 }
 
 function renderPokeCPU(obj){
@@ -51,23 +167,7 @@ function renderPokeCPU(obj){
     display.append(newDisplay)
 }
 
-let random = document.querySelector("#randomizer")
-random.addEventListener('click', randomizePlayer)
-
-function randomizePlayer(){
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=1200&offset=0')
-    .then(res => res.json())
-    .then(data => {
-        let randomPoke = (data.results[Math.floor(Math.random() * data.results.length)])
-        fetch(randomPoke.url)
-        .then(res => res.json())
-        .then(data => renderPokePlayer(data))
-
-    })
-}
-
 function renderPokePlayer(obj){
-
     let received = {...obj}
     let display = document.querySelector("#displayPlayer1")
     display.innerHTML = ""
@@ -87,72 +187,6 @@ function renderPokePlayer(obj){
     newDisplay.appendChild(newPoke)
     display.appendChild(newDisplay)
     
-}
-
-function selector(pokemon){
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-    .then(res => res.json())
-    .then(data => renderPokePlayer(data))
-}
-
-let battle = document.querySelector("#battle")
-battle.addEventListener('click', calculateWinner)
-
-let cpuScore = 0
-let playerScore = 0
-
-function calculateWinner(){
-    let playerScoreContainer = document.querySelector("#playerScore")
-    let cpuScoreContainer = document.querySelector("#cpuScore")
-    let playerPokeType = document.querySelector("#playerType").innerHTML
-    let cpuPokeType = document.querySelector("#cpuType").innerHTML
-    let playerPokeTypeArr = typeChart[playerPokeType]
-    let cpuPokeTypeArr = typeChart[cpuPokeType]
-
-
-    if (cpuWins = playerPokeTypeArr.includes(cpuPokeType)){
-        cpuScore = cpuScore + 1
-        document.querySelector("#displayPlayer1").innerHTML = ""
-        cpuScoreContainer.innerHTML = cpuScore
-        return alert(`You lose: ${cpuPokeType} beats ${playerPokeType}!`)
-    }
-    
-    if (playerWins = cpuPokeTypeArr.includes(playerPokeType)){
-        playerScore = playerScore + 1
-        document.querySelector("#displayCPU").innerHTML = ''
-        playerScoreContainer.innerHTML = playerScore
-        return alert(`You win: ${playerPokeType} beats ${cpuPokeType}!`)
-    }
-
-    if(playerWins === cpuWins){
-        alert('No winner by type. Let\'s check their weights!')
-        let playerWeight = document.querySelector("#playerWeight").innerHTML
-        let cpuWeight = document.querySelector("#cpuWeight").innerHTML
-
-        if(parseInt(playerWeight) > parseInt(cpuWeight)){
-            playerScore = playerScore + 1
-            document.querySelector("#displayCPU").innerHTML = ''
-            playerScoreContainer.innerHTML = playerScore
-            let cpuWKG = (cpuWeight / 10)
-            let playerWKG = (playerWeight / 10)
-            return alert(`You win: ${playerWKG} kg > ${cpuWKG} kg!`)
-        }
-
-        if (parseInt(cpuWeight) > parseInt(playerWeight)){
-            cpuScore = cpuScore + 1
-            document.querySelector("#displayPlayer1").innerHTML = ""
-            cpuScoreContainer.innerHTML = cpuScore
-            let cpuWKG = (cpuWeight / 10)
-            let playerWKG = (playerWeight / 10)
-            return alert(`You lose: ${cpuWKG} kg > ${playerWKG} kg!`)
-        }
-
-        if (parseInt(cpuWeight) === parseInt(playerWeight)){
-            document.querySelector("#displayPlayer1").innerHTML = ""
-            document.querySelector("#displayCPU").innerHTML = ""
-            return alert('What are the odds? No winner by type or weight!')
-        }
-    }
 }
 
 const typeChart = {
